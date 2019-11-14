29bd766f94363b26c6278b07b5d4d4629f271529
Sunil Karki <karkisunil1200@gmail.com>
2019-11-14 14:53:10 -0600

    Creating form

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import FormInput from './components/Form/FormInput/FormInput';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <FormInput />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form/FormInput/FormInput.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {withFormik, Form, Field} from 'formik';
+
+const FormInput = ({name, email, password}) => {
+  return (
+    <div>
+      <h1>Welcome to form</h1>
+      <Form>
+        <Field type='text' name='name' placeholder='Enter your name' />
+        <Field type='text' name='email' placeholder='Enter your email' />
+        <Field type='text' name='password' placeholder='Password goes here' />
+      </Form>
+    </div>
+  );
+};
+
+const FormikUserForm = withFormik({
+  mapPropsToValues({name, email, password}) {
+    return {
+      name: name || ' ',
+      email: email || ' ',
+      password: password || ' '
+    };
+  }
+});
+
+export default FormInput;
