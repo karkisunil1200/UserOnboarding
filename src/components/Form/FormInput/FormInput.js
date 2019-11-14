4404766582550c65f2686a37fee0694ff6103998
Sunil Karki <karkisunil1200@gmail.com>
2019-11-14 16:22:16 -0600

    Done with the mpv

--- src/components/Form/FormInput/FormInput.js
+++ src/components/Form/FormInput/FormInput.js
@@ -3,7 +3,7 @@ import {withFormik, Form, Field} from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 
-const FormInput = ({name, email, password, status}) => {
+const FormInput = ({name, email, password, status, touched, errors}) => {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
@@ -15,11 +15,26 @@ const FormInput = ({name, email, password, status}) => {
       <Form>
         <label>Name</label>
         <Field type='text' name='name' />
+        {touched.name && errors.name && <p>{errors.name}</p>}
+
         <label>Email</label>
         <Field type='text' name='email' />
+        {touched.email && errors.email && <p>{errors.email}</p>}
+
         <label>Password</label>
         <Field type='text' name='password' />
+        {touched.password && errors.password && <p>{errors.password}</p>}
+
+        <button type='submit'>Submit</button>
       </Form>
+      {users.map(user => {
+        return (
+          <ul key={user.id}>
+            <li>User: {user.name}</li>
+            <li>Email: {user.email}</li>
+          </ul>
+        );
+      })}
     </div>
   );
 };
@@ -41,7 +56,7 @@ const FormikFormInput = withFormik({
 
   handleSubmit(values, {setStatus}) {
     axios
-      .post('https://reqres.in/api/users')
+      .post('https://reqres.in/api/users', values)
       .then(res => {
         console.log(res);
         setStatus(res.data);
