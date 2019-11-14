333e4035dd447d772173fda84bf9d30c152b20b9
Sunil Karki <karkisunil1200@gmail.com>
2019-11-14 16:08:52 -0600

    Added submitHandler

--- src/components/Form/FormInput/FormInput.js
+++ src/components/Form/FormInput/FormInput.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {withFormik, Form, Field} from 'formik';
 import * as Yup from 'yup';
+import axios from 'axios';
 
-const FormInput = ({name, email, password}) => {
+const FormInput = ({name, email, password, status}) => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    status && setUsers(animals => [...users, status]);
+  }, [status]);
   return (
     <div>
       <h1>Welcome to form</h1>
@@ -25,13 +31,25 @@ const FormikFormInput = withFormik({
       email: email || ' ',
       password: password || ''
     };
-  }
+  },
 
-  //   validationSchema: Yup.object().shape({
-  //       name: Yup.string().required(),
-  //       email: Yup.string().required(),
-  //       password: Yup.string().required()
-  //   })
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required(),
+    email: Yup.string().required(),
+    password: Yup.string().required()
+  }),
+
+  handleSubmit(values, {setStatus}) {
+    axios
+      .post('https://reqres.in/api/users')
+      .then(res => {
+        console.log(res);
+        setStatus(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 })(FormInput);
 
 export default FormikFormInput;
